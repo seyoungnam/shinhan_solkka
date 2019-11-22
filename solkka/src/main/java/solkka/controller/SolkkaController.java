d4e57a3814d0f68d1d19acd9f6e8304cb5e8037c
Aiden <roottor38@gmail.com>
2019-11-23 00:28:35 +0900

    feat : 디자인 갱신

--- solkka/src/main/java/solkka/controller/SolkkaController.java
+++ solkka/src/main/java/solkka/controller/SolkkaController.java
@@ -1,9 +1,12 @@
 package solkka.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import solkka.service.SolkkaService;
@@ -17,5 +20,16 @@ public class SolkkaController {
 	public JSONObject apiTest(@RequestBody JSONObject body) throws Exception {
 		System.out.println("API TEST");
 		return service.callApi(body);
+	}
+	
+	@PostMapping("/sign")
+	public void singUp(@RequestParam("signUp") String signUp) throws Exception {
+		System.out.println(signUp);
+		System.out.println("singUp TEST");
+	}
+	@PostMapping("/logIn")
+	public void logIn(@RequestParam("logIn") String logIn) throws Exception {
+		System.out.println(logIn);
+		System.out.println("logIn TEST");
 	}
 }
