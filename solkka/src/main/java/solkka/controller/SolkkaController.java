2cd90ba147d7b9d7e6a650083cf1b9de3d7fab55
qus0in <qus0in@gmail.com>
2019-11-22 23:54:03 +0900

    feat : api 실제 연동 구현

--- solkka/src/main/java/solkka/controller/SolkkaController.java
+++ solkka/src/main/java/solkka/controller/SolkkaController.java
@@ -1,10 +1,12 @@
 package solkka.controller;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 
 import solkka.service.SolkkaService;
 
@@ -13,9 +15,10 @@ public class SolkkaController {
 	@Autowired
 	SolkkaService service;
 	
-	@PostMapping("/apiTest")
-	public JSONObject apiTest(@RequestBody JSONObject body) throws Exception {
-		System.out.println("API TEST");
-		return service.callApi(body);
+	@PostMapping("/accountList")
+	public JSONObject accountList(@RequestBody JSONObject body) throws RestClientException, ParseException {
+		System.out.println("// accountList");
+		System.out.println(body);
+		return service.accountList(body.get("number").toString());
 	}
 }
--- solkka/src/main/java/solkka/service/SolkkaService.java
+++ solkka/src/main/java/solkka/service/SolkkaService.java
@@ -1,8 +1,13 @@
 package solkka.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 
 import solkka.model.util.ApiTool;
 
@@ -10,9 +15,20 @@ import solkka.model.util.ApiTool;
 public class SolkkaService {
 	@Autowired
 	ApiTool tool;
-	
-	public JSONObject callApi(JSONObject input) throws Exception {
-		return tool.callApi(input);
+
+	public JSONObject accountList(String number) throws RestClientException, ParseException {
+		System.out.println(number);
+//		{"dataHeader":{},"dataBody":{"serviceCode":"C2010","거래구분":"9","계좌감추기여부":"1","보안계좌조회구분":"2","주민등록번호":"WmokLBDCO9/yfihlYoJFyg=="}}
+		Map<String, Object> dataBodyMap = new HashMap<>();
+		dataBodyMap.put("serviceCode", "C2010");
+		dataBodyMap.put("거래구분", "9");
+		dataBodyMap.put("계좌감추기여부", "1");
+		dataBodyMap.put("보안계좌조회구분", "2");
+		dataBodyMap.put("주민등록번호", number);
+		Map<String, Object> dataMap = new HashMap<>();
+		dataMap.put("dataHeader", new JSONObject());
+		dataMap.put("dataBody", dataBodyMap);
+		return tool.callApi("bank", "accountList", "post", dataMap);
 	}
 
 }
