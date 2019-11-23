3cdd230873cf441ced564f8a8308a9a296fb98f1
qus0in <qus0in@gmail.com>
2019-11-23 16:53:08 +0900

    세부 조정

--- solkka/src/main/java/solkka/service/SolkkaMainService.java
+++ solkka/src/main/java/solkka/service/SolkkaMainService.java
@@ -2,6 +2,7 @@ package solkka.service;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -29,12 +30,13 @@ public class SolkkaMainService {
 	}
 
 	public List<CardDataDTO> getCardData(String userId) throws RestClientException, ParseException, IOException {
-		// API를 호출한다
+		// API를 호출하면서 실제적으로 Sample Data를 호출한다
 		String today = new SimpleDateFormat("yyyyMMdd").format(new Date());
 		String before = new SimpleDateFormat("yyyyMMdd").format(calMonth(new Date(), -3));
+		List<CardDataDTO> result = new ArrayList<>();
+		System.out.println(apiService.usedDebit(before + today));;
+		result.addAll(searchTool.searchIdAndDate("sample_card_credit", "userId", userId, "apprvDate", before));
 		System.out.println(apiService.usedCredit(before + today));;
-		// 대신 Sample Data를 호출한다
-		List<CardDataDTO> result = searchTool.searchIdAndDate("sample_card_credit", "userId", userId, "apprvDate", before);
 		result.addAll(searchTool.searchIdAndDate("sample_card_deposit", "userId", userId, "apprvDate", before));
 		// 최근 순으로 정렬한다
 		result.sort((v1, v2) -> Integer.parseInt(v2.getApprvDate()) - Integer.parseInt(v1.getApprvDate()));
@@ -42,14 +44,17 @@ public class SolkkaMainService {
 	}
 
 	public List<CardDataDTO> getCardDataCouple(String userId1, String userId2) throws IOException, RestClientException, ParseException {
-		// API를 호출한다
+		// API를 호출하면서 실제적으로 Sample Data를 호출한다
 		String today = new SimpleDateFormat("yyyyMMdd").format(new Date());
 		String before = new SimpleDateFormat("yyyyMMdd").format(calMonth(new Date(), -3));
+		List<CardDataDTO> result = new ArrayList<>();
 		System.out.println(apiService.usedCredit(before + today));;
-		// 대신 Sample Data를 호출한다
-		List<CardDataDTO> result = searchTool.searchIdAndDate("sample_card_credit", "userId", userId1, "apprvDate", before);
+		result.addAll(searchTool.searchIdAndDate("sample_card_credit", "userId", userId1, "apprvDate", before));
+		System.out.println(apiService.usedCredit(before + today));;
 		result.addAll(searchTool.searchIdAndDate("sample_card_credit", "userId", userId2, "apprvDate", before));
+		System.out.println(apiService.usedDebit(before + today));;
 		result.addAll(searchTool.searchIdAndDate("sample_card_deposit", "userId", userId1, "apprvDate", before));
+		System.out.println(apiService.usedDebit(before + today));;
 		result.addAll(searchTool.searchIdAndDate("sample_card_deposit", "userId", userId2, "apprvDate", before));
 		// 최근 순으로 정렬한다
 		result.sort((v1, v2) -> Integer.parseInt(v2.getApprvDate()) - Integer.parseInt(v1.getApprvDate()));
